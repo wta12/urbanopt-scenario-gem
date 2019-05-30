3fb41db0ef1fa5a21983d46d0898bf6c005ee2ec
Rawad El Kontar <rawad.elkontar@nrel.gov>
2019-05-30 14:50:56 -0600

    added assertion test for reporting period

--- spec/urbanopt/urbanopt_scenario_reports_spec.rb
+++ spec/urbanopt/urbanopt_scenario_reports_spec.rb
@@ -46,6 +46,8 @@ RSpec.describe URBANopt::Scenario::DefaultReports do
     feature_report_1.program.conditioned_area = 1000
     feature_report_1.program.unconditioned_area = 0
     feature_report_1.program.footprint_area = 1100
+        
+    #feature_report_1.reporting_periods[0][:total_site_energy] = 100
 
     feature_report_2 = URBANopt::Scenario::DefaultReports::FeatureReport.new
     feature_report_2.id = 'feature_2'
@@ -59,6 +61,8 @@ RSpec.describe URBANopt::Scenario::DefaultReports do
     feature_report_2.program.conditioned_area = 1000
     feature_report_2.program.unconditioned_area = 0
     feature_report_2.program.footprint_area = 1100
+        
+    #feature_report_1.reporting_periods[0][:total_site_energy] = 100
 
     scenario = URBANopt::Scenario::ScenarioBase.new('scenario_1', 'Scenario 1', 'scenario_1', nil)
     scenario_report = URBANopt::Scenario::DefaultReports::ScenarioReport.new(scenario)
@@ -109,7 +113,9 @@ RSpec.describe URBANopt::Scenario::DefaultReports do
     existing_costs << URBANopt::Scenario::DefaultReports::ConstructionCost.new({ :category => "HVACComponent", :item_name => "hvac", :unit_cost => 1,
     :cost_units => "CostPerEach", :item_quantity => 1, :total_cost => 1})
     
-  
+    puts "existing_costs = #{existing_costs}"
+    puts "new_costs = #{new_costs}"
+
     construction_cost = URBANopt::Scenario::DefaultReports::ConstructionCost.merge_construction_costs(existing_costs, new_costs)
     
     if construction_cost[0].item_name == "wall"
@@ -135,7 +141,23 @@ RSpec.describe URBANopt::Scenario::DefaultReports do
       expect(construction_cost[2].item_quantity).to eq(1)
       expect(construction_cost[2].total_cost).to eq(1)  
     end
+      
+    puts "final cost = #{existing_costs}"
+
+  end
+
+
+
+  it 'can merge end uses' do
+    existing_end_use = {end_uses: { electricity: { heating: 1, cooling:1 }, natural_gas:{fans: 1, pumps: 1}}}
+    new_end_use = {end_uses: { electricity: { heating: 1, cooling:1 }, natural_gas:{fans: 1, pumps: 1}}}
+
+    end_uses = EndUses.new
+    puts "\nexisting end uses = #{existing_end_use}"
+    puts "\nnew end uses = #{new_end_use}"
     
+    puts "\nfinal end uses = #{end_uses.merge_end_uses(existing_end_use, new_end_use)}"
+
   end
 
   it 'can merge reporting periods together' do
@@ -164,11 +186,46 @@ RSpec.describe URBANopt::Scenario::DefaultReports do
     end_uses: { electricity: { heating: 1, cooling:1, fans:1, pumps: 1} }
     }
     
+    puts "\nexisting periods: #{existing_periods}"
+    puts "\nnew periods: #{new_periods}"
 
-    URBANopt::Scenario::DefaultReports::ReportingPeriod.merge_reporting_periods(existing_periods, new_periods)
+    reporting_period = URBANopt::Scenario::DefaultReports::ReportingPeriod.merge_reporting_periods(existing_periods, new_periods)
+        
+      expect(reporting_period[0][:id]).to eq(5)
+      expect(reporting_period[0][:name]).to eq("Annual")
+      expect(reporting_period[0][:multiplier]).to eq(1)
+      expect(reporting_period[0][:start_date][:month]).to eq(1)
+      expect(reporting_period[0][:start_date][:day_of_month]).to eq(1)
+      expect(reporting_period[0][:start_date][:year]).to eq(2019)
+      expect(reporting_period[0][:end_date][:month]).to eq(12)
+      expect(reporting_period[0][:end_date][:day_of_month]).to eq(31)
+      expect(reporting_period[0][:end_date][:year]).to eq(2019)
+      expect(reporting_period[0][:total_site_energy]).to eq(2)
+      expect(reporting_period[0][:total_source_energy]).to eq(2)
+      expect(reporting_period[0][:end_uses][:electricity][:heating]).to eq(2)
+      expect(reporting_period[0][:end_uses][:electricity][:cooling]).to eq(2)
+      expect(reporting_period[0][:end_uses][:electricity][:fans]).to eq(2)
+      expect(reporting_period[0][:end_uses][:electricity][:pumps]).to eq(2)
+      
+
+      expect(reporting_period[1][:id]).to eq(6)
+      expect(reporting_period[1][:name]).to eq("January")
+      expect(reporting_period[1][:multiplier]).to eq(1)
+      expect(reporting_period[1][:start_date][:month]).to eq(1)
+      expect(reporting_period[1][:start_date][:day_of_month]).to eq(1)
+      expect(reporting_period[1][:start_date][:year]).to eq(2019)
+      expect(reporting_period[1][:end_date][:month]).to eq(1)
+      expect(reporting_period[1][:end_date][:day_of_month]).to eq(31)
+      expect(reporting_period[1][:end_date][:year]).to eq(2019)
+      expect(reporting_period[1][:total_site_energy]).to eq(2)
+      expect(reporting_period[1][:total_source_energy]).to eq(2)
+      expect(reporting_period[1][:end_uses][:electricity][:heating]).to eq(2)
+      expect(reporting_period[1][:end_uses][:electricity][:cooling]).to eq(2)
+      expect(reporting_period[1][:end_uses][:electricity][:fans]).to eq(2)
+      expect(reporting_period[1][:end_uses][:electricity][:pumps]).to eq(2)
     
-    puts existing_periods
-
+    puts "\nfinal periods: #{existing_periods}"
+    
   end
 
 end
