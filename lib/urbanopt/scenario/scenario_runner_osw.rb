d8cba1c0a1c085a2725eb6c6d080123d0a42656e
Rawad El Kontar <rawad.elkontar@nrel.gov>
2019-09-04 20:00:51 -0600

    Implemented staged runner algorithm

--- lib/urbanopt/scenario/scenario_runner_osw.rb
+++ lib/urbanopt/scenario/scenario_runner_osw.rb
@@ -29,8 +29,10 @@
 #*********************************************************************************
 
 require "urbanopt/scenario/scenario_runner_base"
+require 'json'
 
 require 'fileutils'
+#require 'hash_parser'
 
 module URBANopt
   module Scenario
@@ -74,21 +76,67 @@ module URBANopt
       #  @param [Bool] force_clear Clear Scenario before creating SimulationFiles
       #  @return [Array] Returns array of all SimulationFiles, even those created previously, for Scenario
       def run(scenario, force_clear = false)
+        #implement a staged runner here ...building , then trasformers then district systems run 
         runner = OpenStudio::Extension::Runner.new(scenario.root_dir)
 
         simulation_dirs = create_simulation_files(scenario, force_clear)
         
-        osws = []
+        # osws = []
+        # simulation_dirs.each do |simulation_dir|
+        #   if !simulation_dir.is_a?(SimulationDirOSW)
+        #     raise "ScenarioRunnerOSW does not know how to run #{simulation_dir.class}"
+        #   end
+        #   if simulation_dir.out_of_date?
+        #     osws << simulation_dir.in_osw_path
+        #   end
+        # end
+
+        building_osws = []
+        transformer_osws = []
+        district_system_osws = []
+
         simulation_dirs.each do |simulation_dir|
+
+          in_osw = File.read(simulation_dir.in_osw_path)
+          in_osw_hash = JSON.parse(in_osw, :symbolize_names => true)
+
           if !simulation_dir.is_a?(SimulationDirOSW)
             raise "ScenarioRunnerOSW does not know how to run #{simulation_dir.class}"
           end
+
+          # get feature_type value from in.osw files
+          feature_type = nil
+          in_osw_hash[:steps].each{|x| feature_type = x[:arguments][:feature_type] if x[:arguments][:feature_type]}
+
           if simulation_dir.out_of_date?
-            osws << simulation_dir.in_osw_path
+
+            if feature_type == "Building"
+              building_osws << simulation_dir.in_osw_path     
+            elsif feature_type == "District System"
+              district_system_osws << simulation_dir.in_osw_path
+            elsif feature_type == "Transformer"
+              transformer_osws << simulation_dir.in_osw_path
+            else
+              raise "ScenarioRunnerOSW does not know how to run a #{feature_type} feature"
+            end
+            
           end
         end
-        
-        failures = runner.run_osws(osws)
+
+
+        # failures
+        failures = []
+
+        # run building_osws
+        building_failures = runner.run_osws(building_osws)
+        failures << building_failures
+        # run district_system_osws
+        district_system_failures = runner.run_osws(district_system_osws)
+        failures << district_system_failures
+        # run transformer_osws
+        transformer_failures = runner.run_osws(transformer_osws)
+        failures << transformer_failures
+
         #puts "failures = #{failures}"
         return simulation_dirs
       end
