62adb4e8e3aec87670be483addc458dd619b3d9e
kflemin <katherine.fleming@nrel.gov>
2020-08-20 21:35:44 -0600

    ensure that mapper classes are added in a particular order

--- lib/urbanopt/scenario/scenario_csv.rb
+++ lib/urbanopt/scenario/scenario_csv.rb
@@ -52,7 +52,6 @@ module URBANopt
 
       def initialize(name, root_dir, run_dir, feature_file, mapper_files_dir, csv_file, num_header_rows)
         super(name, root_dir, run_dir, feature_file)
-
         @mapper_files_dir = mapper_files_dir
         @csv_file = csv_file
         @num_header_rows = num_header_rows
@@ -73,7 +72,19 @@ module URBANopt
 
       # Require all simulation mappers in mapper_files_dir
       def load_mapper_files
-        Dir.glob(File.join(@mapper_files_dir, '/*.rb')).each do |f|
+        dirs = Dir.glob(File.join(@mapper_files_dir, '/*.rb'))
+        # order is not guaranteed...make sure to add Baseline first, then High Efficiency
+        ordered_dirs = []
+        bindex = dirs.find_index {|i| i.include? "Baseline.rb"}
+        ordered_dirs << dirs[bindex]
+        dirs.delete_at(bindex)
+        hindex = dirs.find_index {|i| i.include? "HighEfficiency.rb"}
+        ordered_dirs << dirs[hindex]
+        dirs.delete_at(hindex)
+        # then the rest
+        ordered_dirs = ordered_dirs + dirs
+
+        ordered_dirs.each do |f|
           begin
             require(f)
           rescue LoadError => e
